7f8cfb44f92b2398ce264525e87f109f2eff84a8
farroshayray <farros.hr@gmail.com>
2024-12-10 19:56:22 +0700

    update home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 // pages/home/index.tsx
 import React, { useState, useEffect } from "react";
 import Navbar from "@/components/ui/navbar";
-import PromoCarousel from "./promo_carousel";
+import CategoryCarousel from "../../components/ui/category_carousel";
 import MarketList from "./market_list";
 import CategoryList from "./category_list";
 import ProductGrid from "./product_grid";
@@ -25,18 +25,25 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <Home_topupBalance />
-      <div className="market-list-container flex flex-col mt-5 p-10">
-        <h1 className="mt-3 mb-3 mx-auto text-xl">Daftar Pasar</h1>
+      <div className="mt-4 py-4 flex flex-col rounded-xl p-1 sm:flex-col md:flex-row h-96">
+        <div className="md:w-1/4 md:mr-2"><Home_topupBalance /></div>
+        <div className="md:w-3/4 p-2 rounded-xl bg-white bg-opacity-20">
+          <p>Kategori</p>
+          <div><CategoryCarousel /></div>
+        </div>
+        
+      </div>
+      <div className="market-list-container flex flex-col p-10 mt-32 md:mt-0 bg-white bg-opacity-20 rounded-xl">
+        <h1 className="mt-3 mb-3 mx-auto text-3xl">Daftar Pasar</h1>
         <MarketList />
       </div>
-      <div className="product-list-by-radius flex flex-col mt-5 rounded-xl bg-gray-800 p-10">
-        <h1 className="mt-3 mb-3 mx-auto text-xl">Kategori Pilihan</h1>
+      <div className="product-list-by-radius flex flex-col mt-5 rounded-xl bg-white bg-opacity-20 p-10">
+        <h1 className="mt-3 mb-3 mx-auto text-3xl">Kategori Pilihan</h1>
         <CategoryList categoryId={2} />
         <CategoryList categoryId={3} />
       </div>
-      <div className="product-grid flex flex-col mt-5 rounded-xl bg-gray-800 p-10">
-        <h1 className="mt-3 mb-3 mx-auto text-xl">Semua Produk</h1>
+      <div className="product-grid flex flex-col mt-5 rounded-xl bg-white bg-opacity-20 p-10">
+        <h1 className="mt-3 mb-3 mx-auto text-3xl">Semua Produk</h1>
         <ProductGrid />
       </div>
     </div>
